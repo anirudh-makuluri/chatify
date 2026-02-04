7aea101e21c9a0938fc923e5ae44ee1d285738d4
amakulur <anirudh.makuluri@gmail.com>
2026-02-03 21:17:06 -0700

    upgraded to latest react version

--- metro.config.js
+++ metro.config.js
@@ -4,5 +4,7 @@ const { withNativeWind } = require('nativewind/metro');
 const config = getDefaultConfig(__dirname, { isCSSEnabled: true })
 
 config.resolver.sourceExts.push('cjs');
+config.resolver.unstable_enablePackageExports = false;
+
 
 module.exports = withNativeWind(config, { input: './global.css' })
